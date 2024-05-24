6de792d315ab3c038449ffa07a3b98e1e75b3cee
PatricioPena <Patricio.17po.@gmail.com>
2024-05-23 19:58:11 -0600

    Front adj

--- src/components/HubComponent.vue
+++ src/components/HubComponent.vue
@@ -11,15 +11,15 @@
         <v-col>
           <ServiceComponent
             class="w-100"
-            :name="'Google'"
-            :imageUrl="'https://upload.wikimedia.org/wikipedia/commons/0/07/Wikipedia_logo_%28svg%29.svg'"
+            :name="'Google Académico'"
+            :imageUrl="'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAHiotweovbY-VYLheHXIjgJ1_USrBghmkldIrLDkELw&s'"
           ></ServiceComponent>
         </v-col>
         <v-col>
           <ServiceComponent
             class="w-100"
-            :name="'Wikipedia'"
-            :imageUrl="'https://upload.wikimedia.org/wikipedia/commons/0/07/Wikipedia_logo_%28svg%29.svg'"
+            :name="'Google Books'"
+            :imageUrl="'https://i.blogs.es/c5141d/play-books/1366_2000.jpg'"
           ></ServiceComponent>
         </v-col>
       </v-row>
@@ -29,22 +29,22 @@
         <v-col class="">
           <ServiceComponent
             class="w-100"
-            :name="'Wikipedia'"
-            :imageUrl="'https://upload.wikimedia.org/wikipedia/commons/0/07/Wikipedia_logo_%28svg%29.svg'"
+            :name="'ERIC (Education Resources Information Center)'"
+            :imageUrl="'https://upload.wikimedia.org/wikipedia/commons/5/5b/ERIC_logo_%28US_ED%29.png'"
           ></ServiceComponent>
         </v-col>
         <v-col>
           <ServiceComponent
             class="w-100"
-            :name="'Wikipedia'"
-            :imageUrl="'https://upload.wikimedia.org/wikipedia/commons/0/07/Wikipedia_logo_%28svg%29.svg'"
+            :name="'WorldCat Search API (OCLC)'"
+            :imageUrl="'https://developer.api.oclc.org/assets/header-logo-DmMTs40y.png'"
           ></ServiceComponent>
         </v-col>
         <v-col>
           <ServiceComponent
             class="w-100"
-            :name="'Wikipedia'"
-            :imageUrl="'https://upload.wikimedia.org/wikipedia/commons/0/07/Wikipedia_logo_%28svg%29.svg'"
+            :name="'EBSCOhost SOAP Web Service'"
+            :imageUrl="'https://minitex.umn.edu/sites/default/files/styles/manual_crop_16_9/public/images/2020-01/EBSCOhost_logo_CMYK%5B1%5D.png?h=1b4c7e77&itok=ujsGeW0Z'"
           ></ServiceComponent>
         </v-col>
       </v-row>
@@ -52,7 +52,7 @@
 </template>
 
 <script lang="ts" setup>
-import ServiceComponent from "./ServiceComponente";
+import ServiceComponent from "./ServiceComponente.vue";
 </script>
 
 <style scoped>
