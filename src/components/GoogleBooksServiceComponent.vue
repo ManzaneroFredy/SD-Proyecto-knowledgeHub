d32d069b0120ef56faf2ce1d2871f585c5fd451d
Patricio P <patriciopenaojeda@gmail.com>
2024-05-24 16:47:03 -0600

    Google Books api

--- src/components/GoogleBooksServiceComponent.vue
+++ src/components/GoogleBooksServiceComponent.vue
@@ -4,48 +4,69 @@
     <v-container>
       <v-form ref="form" v-model="valid" lazy-validation>
         <v-row>
-          <v-col cols="12" md="6" v-for="(field, index) in fields" :key="index">
-            <h3>{{ field.label }}</h3>
+          <v-col cols="12" md="6">
+            <h3>Introduce el título</h3>
             <v-text-field
-              v-model="field.value"
-              :label="field.label"
-              :rules="field.rules"
+              v-model="title"
+              label="Introduce el título"
+              :rules="[rules.required]"
               required
             ></v-text-field>
           </v-col>
         </v-row>
         <v-btn :disabled="!valid" @click="submitForm">Submit</v-btn>
       </v-form>
-      <GoogleBooksServiceResultsComponent v-if="showResults" :results="fields" />
+      <GoogleBooksServiceResultsComponent v-if="showResults" :results="transformedResults" />
     </v-container>
   </div>
 </template>
 
 <script setup>
-import { ref, reactive } from 'vue';
+import { ref, computed } from 'vue';
 import GoogleBooksServiceResultsComponent from './GoogleBooksServiceResultsComponent';
 
 const valid = ref(false);
 const showResults = ref(false);
+const title = ref('');
+const results = ref([]);
 
-const fields = reactive([
-  { label: 'Introduce el título', value: '', rules: [(v) => !!v || 'Field 1 is required'] },
-]);
+const rules = {
+  required: v => !!v || 'El campo es obligatorio'
+};
 
-// GET https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=yourAPIKey
-
-const submitForm = () => {
+const submitForm = async () => {
   if (valid.value) {
+    await fetchBooks(title.value);
     showResults.value = true;
   }
 };
+
+const fetchBooks = async (query) => {
+  const apiKey = "AIzaSyDXQDfS8hLzJiOqhUaARycLfxluD7y3zWY";
+  const url = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(query)}&key=${apiKey}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    results.value = data.items || [];
+  } catch (error) {
+    console.error('Error fetching books:', error);
+  }
+};
+
+const transformedResults = computed(() => {
+  return results.value.map(book => ({
+    label: book.volumeInfo.title,
+    value: book.volumeInfo.authors ? book.volumeInfo.authors.join(', ') : 'Unknown author'
+  }));
+});
 </script>
 
 <style scoped>
-.title{
+.title {
   text-align: center;
 }
-.ServiceHeader{
+.ServiceHeader {
   padding-top: 50px;
 }
 </style>
